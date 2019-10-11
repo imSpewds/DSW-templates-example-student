bda346902ca77c794cfabff8ab33ae725de70bad
imSpewds <49460156+imSpewds@users.noreply.github.com>
2019-10-11 09:09:52 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,17 +6,13 @@ app = Flask(__name__)
 def render_main():
     return render_template('home.html')
 
-@app.route("/aboutus") #annotations tell which function goes with which request
+@app.route("/p1") #annotations tell which function goes with which request
 def render_page1():
     return render_template('page1.html')
 
 @app.route("/p2")
 def render_page2():
     return render_template('page2.html')
-
-@app.route("/p3")
-def render_page3():
-    return render_template('page3.html')
     
 if __name__=="__main__":
     app.run(debug=False)
