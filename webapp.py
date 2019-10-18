e317cb3b9e031c8ef7f6c891e1fb66672a1a7b8c
imSpewds <titorific10@gmail.com>
2019-10-18 09:20:03 -0700

    finished i hope

--- webapp.py
+++ webapp.py
@@ -1,11 +1,11 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def render_main():
     return render_template('home.html')
-
+    
 @app.route("/p1") #annotations tell which function goes with which request
 def render_page1():
     return render_template('page1.html')
@@ -16,12 +16,23 @@ def render_page2():
 
 @app.route("/response")
 def render_response():
-    favorite_color = request.args['color'] #get user's input for color input
-    if favorite_color == "red":
-        response = "Thats my favorite color too!"
-    else:
-        response = "My favorite color is red."
+    USD = request.args['USD']
+    response = float(USD) * 7.08
+    
     return render_template('response.html', responseFromServer=response)
     
+@app.route("/response2")
+def render_response2():
+    Miles = request.args['Miles']
+    response = float(Miles) * 1.609
+    
+    return render_template('response2.html', responseFromServer=response)
+    
+@app.route("/response3")
+def render_response3():
+    Peso = request.args['Peso']
+    response = float(Peso) * 0.052
+    
+    return render_template('response3.html', responseFromServer=response)
 if __name__=="__main__":
     app.run(debug=False)
